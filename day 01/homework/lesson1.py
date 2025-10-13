2f4b8525f1cfa21645a6294c4aec93302c5265fc
natu6489 <natia6489@gmail.com>
2025-10-13 22:07:55 +0400

    commit

---
+++ day 01/homework/lesson1.py
@@ -0,0 +1,17 @@
+print("hello world")
+
+#print aris funqcia frchxilebi ki gvwirdeba am funqciiis gamosadzaxeblad
+
+
+name = "natia"
+print(name)
+
+#deklaracia aris cvladis sheqmna
+
+number = 10
+print(number)
+
+number = 20
+print(number)
+
+#aq funqciam terminalshi gamoitana cvladis is mnishvneloba romelic bolos mivaniwet am cvlads
---
+++ day 02/classwork/main.py
@@ -0,0 +1,16 @@
+print(12+34)
+print(242+275)
+print(463+88)
+
+print(3748-567)
+print(278-126)
+print(789-256)
+
+print(372*289)
+print(2748*829)
+print(2849*2894)
+
+print(700/10)
+print(570/5)
+print(324/9)
+
