410fd618d8d1b5934e66645c63b2368bd3e61d85
natu6489 <natia6489@gmail.com>
2025-10-22 14:48:27 +0400

    'commit'

---
+++ day 06/classwork/homework/main.py
@@ -0,0 +1,19 @@
+#input არის ნებისმიერი ინფორმაცია რაც კომპიუტერში შედის  მაგ: მიკროფონი, კლავიატურა, მაუსი 
+#output არის ნებისმიერი ინფორმაცია რომელიც კომპიუტერიდან გამოდის   მაგ: ეკრანი, ყურსასმენი, დინამიკი 
+
+
+
+name = input("enter your name:  ")
+surname = input("enter your surname:  ")
+print(name + surname)
+
+
+a = int(input("enter the first number:  "))
+b = int(input("enter the second number:  "))
+c = int(input("enter the third number:  "))
+d = int(input("enter the fourth number:  "))
+e = int(input("enter the fifth number:  "))
+
+average = (a+b+c+d+e) / 5
+
+print(average)
---
+++ day 06/classwork/main.py
@@ -0,0 +1,10 @@
+name = input("please enter your name:  ")
+print(name)
+
+
+number = input("please enter a number:  ")
+NUMBER = input("please enter a number:  ")
+print(number + NUMBER)
+
+#print ფუნქციას იმიტომ არ გამოაქვს მათი ჯამი, რომ input() ფუნქცია აბრუნებს ტექსტს (string), და არა რიცხვს (integer).
+
